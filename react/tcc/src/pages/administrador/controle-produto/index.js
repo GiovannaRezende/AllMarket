dfb8002570996f238a87aaf5834bd5a2e694bc2d
GiovannaRezende <giovannadossantos2004@gmail.com>
2021-09-28 21:17:55 -0300

    ok-ok

--- react/tcc/src/pages/administrador/controle-produto/index.js
+++ react/tcc/src/pages/administrador/controle-produto/index.js
@@ -5,7 +5,76 @@ export default function Index() {
     return(
         <ControleProdutos>
             <CabecalhoAdm/>
-
+                <div class="corpo">
+                    <div class="corpo-pt1">Controle de Produtos</div>
+                    <div class="corpo-pt2">
+                        <div class="box">
+                            <div class="esquerda">
+                                <div class="imagem"><img src="../../react/tcc/public/assets/images/Imagem-Produto.svg" alt=""/></div>
+                                <div class="textos">
+                                    <div class="produto">Desodorante - Rexona</div>
+                                    <div class="peso">400ml</div>
+                                </div>
+                            </div>
+                            <div class="botoes">
+                                <div class="botao1"><button>Editar</button></div>
+                                <div class="botao2"><button>Deletar</button></div>
+                            </div>
+                        </div>
+                        <div class="box">
+                            <div class="esquerda">
+                                <div class="imagem"><img src="../../React/tcc/public/Assets/Images/Imagem-Produto.svg" alt=""/></div>
+                                <div class="textos">
+                                    <div class="produto">Desodorante - Rexona</div>
+                                    <div class="peso">400ml</div>
+                                </div>
+                            </div>
+                            <div class="botoes">
+                                <div class="botao1"><button>Editar</button></div>
+                                <div class="botao2"><button>Deletar</button></div>
+                            </div>
+                        </div>
+                        <div class="box">
+                            <div class="esquerda">
+                                <div class="imagem"><img src="../../React/tcc/public/Assets/Images/Imagem-Produto.svg" alt=""/></div>
+                                <div class="textos">
+                                    <div class="produto">Desodorante - Rexona</div>
+                                    <div class="peso">400ml</div>
+                                </div>
+                            </div>
+                            <div class="botoes">
+                                <div class="botao1"><button>Editar</button></div>
+                                <div class="botao2"><button>Deletar</button></div>
+                            </div>
+                        </div>
+                        <div class="box">
+                            <div class="esquerda">
+                                <div class="imagem"><img src="../../React/tcc/public/Assets/Images/Imagem-Produto.svg" alt=""/></div>
+                                <div class="textos">
+                                    <div class="produto">Desodorante - Rexona</div>
+                                    <div class="peso">400ml</div>
+                                </div>
+                            </div>
+                            <div class="botoes">
+                                <div class="botao1"><button>Editar</button></div>
+                                <div class="botao2"><button>Deletar</button></div>
+                            </div>
+                        </div>
+                        <div class="box">
+                            <div class="esquerda">
+                                <div class="imagem"><img src="../../React/tcc/public/Assets/Images/Imagem-Produto.svg" alt=""/></div>
+                                <div class="textos">
+                                    <div class="produto">Desodorante - Rexona</div>
+                                    <div class="peso">400ml</div>
+                                </div>
+                            </div>
+                            <div class="botoes">
+                                <div class="botao1"><button>Editar</button></div>
+                                <div class="botao2"><button>Deletar</button></div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
         </ControleProdutos>  
     ) 
 }
--- react/tcc/src/pages/administrador/controle-produto/styled.js
+++ react/tcc/src/pages/administrador/controle-produto/styled.js
@@ -1,6 +1,89 @@
 import styled from 'styled-components'
 
 const ControleProdutos = styled.div`
+.corpo {
+    display: flex;
+    flex-direction: column;
+    padding: 2em;
+}
+
+.corpo-pt1 {
+    font: 1.5em Roboto;
+    font-weight: bold;
+    padding: 0em 0em .8em 0em;
+}
+
+.corpo-pt2 {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+}
+
+.box {
+    display: flex;
+    flex-direction: row;
+    box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+    width: 100%;
+    border-radius: .7em;
+    background-color: #f9f9f9;
+    padding: 1em;
+    justify-content: space-between;
+    height: 3em;
+    margin: .5em 0em;
+}
+
+.esquerda {
+    display: flex;
+    flex-direction: row;
+}
+
+.imagem img {
+    width: 3em;
+    height: 3.2em;
+    margin-right: .7em;
+}
+
+.textos {
+    display: flex;
+    flex-direction: column;
+
+}
+
+.produto {
+    font: 900 1em Roboto;
+    line-height: 1.6em;
+}
+
+.peso {
+    font: 500 .8em Roboto;
+}
+
+.botoes {
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+}
+
+.botao1 button {
+    border: none;
+    color: white;
+    background-color: #00F90A;
+    padding: .7em 1.5em;
+    border-radius: .7em;
+    font: 800 .8em Roboto;
+    box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+}
+
+.botao2 button {
+    border: none;
+    color: white;
+    background-color: #F90000;
+    padding: .7em 1.5em;
+    border-radius: .7em;
+    font: 800 .8em Roboto;
+    box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+    margin-left: .7em;
+}
 
 `
 
