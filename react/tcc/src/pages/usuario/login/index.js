696925903d39073d48cccadfb07ad772bf07c049
GiovannaRezende <giovannadossantos2004@gmail.com>
2021-09-29 13:37:23 -0300

    ok

---
+++ react/tcc/src/pages/usuario/login/index.js
@@ -0,0 +1,10 @@
+import { LoginUsuarioStyled }  from './styled';
+import BotaoAzul from '../../../components/outros/botao/botao';
+
+export default function CadastroUsuario() {
+    return(
+        <LoginUsuarioStyled>
+            
+        </LoginUsuarioStyled>  
+    ) 
+}
