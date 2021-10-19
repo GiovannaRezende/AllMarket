64c34389f66c1b522d5c7508108297a2da48fd0b
Giovanna <giovannadossantos2004@gmail.com>
2021-10-19 17:51:17 -0300

    ok tabelas

--- react/tcc/src/pages/administrador/controle-produto/index.js
+++ react/tcc/src/pages/administrador/controle-produto/index.js
@@ -1,7 +1,6 @@
 import CabecalhoAdm from '../../../components/cabecalhos/admin/cabecalho-admin';
 import { ControleProdutosStyled }  from './styled';
 
-
 export default function ControleProduto() {
     return(
         <ControleProdutosStyled>
