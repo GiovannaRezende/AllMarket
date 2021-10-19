9cbb8d1754ef5f93e7cf5c265e6ae5577f3a83e3
Giovanna <giovannadossantos2004@gmail.com>
2021-10-19 18:16:05 -0300

    ok

--- api/src/index.js
+++ api/src/index.js
@@ -4,5 +4,39 @@ import cors from 'cors'
 const app = express();
 app.use(cors());
 
+app.get('/produtos', async (req, resp) => {
+    try {
+        let produtos = await db.infoc_tct_produto.findAll();
+        resp.send(produtos);
+    } catch (e) {
+        resp.send({ erro: e.toString()})
+    }
+})
+
+app.post('/produtos', async (req, resp) => {
+    try {
+        let m = await db.infoc_tct_produto.findOne({ where: { nm_produto: produto }});
+
+        if (m != null)
+            return resp.send({ erro: 'Produto já existe!' });
+
+        let r = await db.infoc_tct_produto.create({
+            nm_categoria: categoria,
+            nm_produto: produto,
+            nr_codigo: codigo,
+            ds_setor: setor,
+            ds_embalagem: embalagem,
+            nm_marca: marca,
+            ds_peso: peso,
+            ds_descricao: descricao,
+            vl_preco: preco
+        })
+        resp.send(r);
+    } catch (e) {
+        resp.send({erro: e.toString()})
+    }
+})
+
+
 
 app.listen(process.env.PORT, x => console.log(`Server up at port ${process.env.PORT}`));
--- react/tcc/src/service/api.js
+++ react/tcc/src/service/api.js
@@ -2,3 +2,16 @@ import axios from 'axios'
 const api = new axios.create({
     baseURL: 'http://localhost:3030'
 })
+
+
+export default class Api {
+    async listar() {
+        let r = await api.get('/produtos');
+        return r.data;
+    }
+
+    async inserir(categoria, produto, codigo, setor, embalagem, marca, peso, descricao, preco) {
+        let r = await api.post('/produtos', { categoria, produto, codigo, setor, embalagem, marca, peso, descricao, preco } );
+        return r.data;
+    }
+}
