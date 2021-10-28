4b99a3e65140259e6690837f8f0258e927f3f9d5
NickNunesAndrade <89866769+NickNunesAndrade@users.noreply.github.com>
2021-10-28 17:11:56 -0300

    api - cartao

--- react/tcc/src/pages/usuario/cadastro-cartao/index.js
+++ react/tcc/src/pages/usuario/cadastro-cartao/index.js
@@ -2,8 +2,32 @@ import CabecalhoStyledUsu from '../../../components/cabecalhos/admin/cabecalho-a
 import { CadastroCartaoStyled }  from './styled';
 import { BotaoLaranja } from '../../../components/outros/botoes/styled';
 import { InputCadastro } from '../../../components/outros/inputs/input';
+import { useState } from 'react';
+import Api from '../../../service/api';
+const api = new Api();
 
 export default function CadastroCartao() {
+    const [dono, setDono] = useState('');
+    const [cartao, setCartao] = useState('');
+    const [tipo, setTipo] = useState('');
+    const [validade, setValidade] = useState('');
+    const [cvv, setCvv] = useState('');
+
+    async function adiciona_cartao()  {
+        let r = await api.adicionar_cartao(dono, cartao, tipo, validade, cvv);
+        alert('Cartao cadastrado !!');
+        limpar();
+    }
+
+    function limpar() {
+        setDono('');
+        setCartao('');
+        setTipo('');
+        setValidade('');
+        setCvv('');
+    }
+
+
     return(
         <CadastroCartaoStyled>
             <CabecalhoStyledUsu/>
@@ -13,32 +37,29 @@ export default function CadastroCartao() {
                     <div class="container-conteudo-1">
                         <div class="input-1">
                             <div class="input-texto">Nome do Cartão</div>
-                            <InputCadastro></InputCadastro>
+                            <InputCadastro value={dono} onChange={e => setDono(e.target.value)}></InputCadastro>
                         </div>
                         <div class="input-2">
                             <div class="input-texto">Número do Cartão</div>
-                            <InputCadastro></InputCadastro>
+                            <InputCadastro value={cartao} onChange={e => setCartao(e.target.value)}></InputCadastro>
                         </div>
                         <div class="input-3">
                             <div class="input-texto">Tipo de Cartão</div>
-                            <select name="cars" id="cars" form="carform">
-                                <option>MasterCard</option>
-                                <option>Visa</option>
-                              </select>
+                            <InputCadastro value={tipo} onChange={e => setTipo(e.target.value)} ></InputCadastro>
                         </div>
                         <div class="input-4">
                             <div class="input-texto">Data de Validade</div>
-                            <InputCadastro></InputCadastro>
+                            <InputCadastro value={validade} onChange={e => setValidade(e.target.value)} ></InputCadastro>
                         </div>
                     </div>
                     <div class="container-conteudo-2">
                         <div class="input-5">
                             <div class="input-texto">CVV</div>
-                            <InputCadastro></InputCadastro>
+                            <InputCadastro value={cvv} onChange={e => setCvv(e.target.value)}></InputCadastro>
                         </div>
                     </div>
                 </div>
-                <div class="botao-cadastro"><BotaoLaranja>Cadastrar Cartão</BotaoLaranja></div>
+                <div class="botao-cadastro"><BotaoLaranja onClick={adiciona_cartao} >Cadastrar Cartão</BotaoLaranja></div>
             </div>
         </CadastroCartaoStyled>  
     ) 
