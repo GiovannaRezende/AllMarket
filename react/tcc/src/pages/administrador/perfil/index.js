ced2a146cb298afad253eeacd0aa738c9b22a604
GiovannaRezende <giovannadossantos2004@gmail.com>
2021-11-04 23:42:48 -0300

    ok redirects

--- react/tcc/src/pages/administrador/perfil/index.js
+++ react/tcc/src/pages/administrador/perfil/index.js
@@ -5,22 +5,22 @@ export default function PerfilAdm() {
     return(
         <PerfilAdmStyled>
             <CabecalhoAdm/>
-            <div class="conteudo">
-            <div class="admin-infos">
-                <div class="cab-infos">
-                    <div class="foto-perfil"><img src="/assets/images/Perfil-Admin.svg" alt=""/></div>
+            <div className="conteudo">
+            <div className="admin-infos">
+                <div className="cab-infos">
+                    <div className="foto-perfil"><img src="/assets/images/Perfil-Admin.svg" alt=""/></div>
                 </div>
-                <div class="info"><span>Nome:</span>João da Silva<button className="editar"><img src="/assets/images/Ícone-Editar.png" alt=""/></button></div>
-                <div class="info"><span>CPF:</span>111.222.333-00<button className="editar"><img src="/assets/images/Ícone-Editar.png" alt=""/></button></div>
-                <div class="info"><span>Email:</span>joaosilva@gmail.com<button className="editar"><img src="/assets/images/Ícone-Editar.png" alt=""/></button></div>
-                <div class="info"><span>Telefone:</span>(11)99990-7272<button className="editar"><img src="/assets/images/Ícone-Editar.png" alt=""/></button></div>
-                <div class="info"><span>Cargo:</span>Administrador<button className="editar"><img src="/assets/images/Ícone-Editar.png" alt=""/></button></div>
+                <div className="info"><span>Nome:</span>João da Silva<button className="editar"><img src="/assets/images/Ícone-Editar.png" alt=""/></button></div>
+                <div className="info"><span>CPF:</span>111.222.333-00<button className="editar"><img src="/assets/images/Ícone-Editar.png" alt=""/></button></div>
+                <div className="info"><span>Email:</span>joaosilva@gmail.com<button className="editar"><img src="/assets/images/Ícone-Editar.png" alt=""/></button></div>
+                <div className="info"><span>Telefone:</span>(11)99990-7272<button className="editar"><img src="/assets/images/Ícone-Editar.png" alt=""/></button></div>
+                <div className="info"><span>Cargo:</span>Administrador<button className="editar"><img src="/assets/images/Ícone-Editar.png" alt=""/></button></div>
             </div>
-            <div class="admin-funcoes">
-                <div class="funcoes"><button> <a href="/controle-produto"> Administrar produtos </a> </button></div>
-                <div class="funcoes"><button> <a href="/aprovar-compra"> Aprovar compras </a> </button> </div>
-                <div class="funcoes"><button> <a href="/mensagens"> Responder mensagens </a> </button> </div>
-                <div class="funcoes"><button> <a href="/"> Modo usuário </a> </button> </div>
+            <div className="admin-funcoes">
+                <div className="funcoes"><button> <a href="/controle-produto"> Administrar produtos </a> </button></div>
+                <div className="funcoes"><button> <a href="/aprovar-compra"> Aprovar compras </a> </button> </div>
+                <div className="funcoes"><button> <a href="/mensagens"> Responder mensagens </a> </button> </div>
+                <div className="funcoes"><button> <a href="/"> Modo usuário </a> </button> </div>
             </div>
         </div>
         </PerfilAdmStyled>
