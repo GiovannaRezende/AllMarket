bcf51d356197c83cf763784e71a9aed927764d25
GiovannaRezende <giovannadossantos2004@gmail.com>
2021-11-07 00:48:11 -0300

    ok

--- react/tcc/src/pages/navegacao/styled.js
+++ react/tcc/src/pages/navegacao/styled.js
@@ -1,13 +1,10 @@
 import styled from "styled-components";
 
 const Container = styled.div `
-
-
     display: flex;
     flex-direction: column;
     text-align: center;
     font-family: Roboto;
-
 
 .titulo {
     font: 900 2em Roboto;
