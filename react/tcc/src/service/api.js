264a257270e7c790bd25e92fc2e998e664ebf9e0
NickNunesAndrade <89866769+NickNunesAndrade@users.noreply.github.com>
2021-11-11 22:03:16 -0300

    quase terminandoo

--- react/tcc/src/service/api.js
+++ react/tcc/src/service/api.js
@@ -1,12 +1,12 @@
-import axios from 'axios'
+import axios from ‘axios’
 const api = new axios.create({
-    baseURL: 'https://allmarket-tcc.herokuapp.com'
+baseURL: ‘http://localhost:3030’
 })
 
 export default class Api {
     async listar() {
-        let r = await api.get('/produtos');
-        return r.data;
+    let r = await api.get(‘/produtos’);
+    return r.data;
     }
 
     async inserir(categoria, produto, codigo, embalagem, marca, peso, descricao, preco) {
@@ -33,7 +33,7 @@ export default class Api {
         let r = await api.put('/endereco/' + { status } );
         return r.data;
     }
-    
+
     async adicionarEndereco(cep, estado, cidade, rua, numero, complemento, referencia) {
         let r = await api.post('/endereco', {cep, estado, cidade, rua, numero, complemento, referencia});
         return r.data;
@@ -56,7 +56,8 @@ export default class Api {
 
     async chat(cliente, administrador, mensagem) {
         let r = await api.post('/chat', { cliente, administrador, mensagem });
-        return r.data;
+        return r.data
+        ;
     }
 
     async inserirUsuario(login, senha, cpf, email, ) {
