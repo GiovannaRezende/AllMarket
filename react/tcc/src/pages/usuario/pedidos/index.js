01496c5051999fd1a6af13c0ac7623520e81b8b4
Giovanna <giovannadossantos2004@gmail.com>
2021-11-12 16:38:06 -0300

    ok

--- react/tcc/src/pages/usuario/pedidos/index.js
+++ react/tcc/src/pages/usuario/pedidos/index.js
@@ -18,31 +18,30 @@ export default function Pedido() {
     useEffect(() => {
         listarPedidos();
     }, [])
-    
 
     return (
         <PedidosStyled>
             <CabecalhoUsu/>
             <div class="conteudo">
-                <div class="numero-pedido"> 
-                    <div class="pedido"> Pedido 0845 </div> 
-                    <div class="barra"></div>
-                    <div class="status-pedido"> Pedido Entregue </div> 
+                <div className="numero-pedido"> 
+                    <div className="pedido"> Pedido 0845 </div> 
+                    <div className="barra"></div>
+                    <div className="status-pedido"> Pedido Entregue </div> 
                 </div>
-                <div class="texto-lista"> Lista de Produtos </div> 
-                <div class="box-lista">
+                <div className="texto-lista"> Lista de Produtos </div> 
+                <div className="box-lista">
                     {pedidos.map((item, i) =>
-                        <div class="box-produto">
-                        <div class="imagem-produto"> <img src="/assets/images/coca-cola.png" alt=""/> </div> 
-                        <div class="informacoes-produto"> 
-                            <div class="nome-produto"></div>
-                            <div class="preco"> <b>Preço: </b> R${item.vl_total}</div>
-                            <div class="qtd-produto"> <b>Quantidade selecionada: </b> 2 </div>
+                        <div className="box-produto">
+                        <div className="imagem-produto"> <img src="/assets/images/coca-cola.svg" alt=""/> </div> 
+                        <div className="informacoes-produto"> 
+                            <div className="nome-produto"></div>
+                            <div className="preco"> <b>Preço: </b>R${item.vl_total}</div>
+                            <div className="qtd-produto"> <b>Quantidade selecionada: </b>{item.qtd_quantidade}</div>
                         </div>
                     </div>
                     )}
                 </div>
-                <div class="botao-voltar"> <button>Voltar</button> </div>
+                <div className="botao-voltar"><button>Voltar</button></div>
             </div> 
         </PedidosStyled>
     )
